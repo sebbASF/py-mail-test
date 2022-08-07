d6a2466d7a58215d99975851ac58e5f438b55141
Sebb <sebb@apache.org>
2022-08-07 11:41:00 +0100

    Oops, no space before optional info

--- mailbox.py
+++ mailbox.py
@@ -841,7 +841,7 @@ class mbox(_mboxMMDF):
     DAY_RE = b' (?:Mon|Tue|Wed|Thu|Fri|Sat|Sun)'
     MON_RE = b' (?:Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)'
     DTY_RE = b' [ 0]\\d \\d\\d:\\d\\d:\\d\\d \\d{4}' # day, time, year
-    STRICT_RE = b'From \\S+' + DAY_RE + MON_RE + DTY_RE + b' ( .+)?' + linesep + b'\\Z'
+    STRICT_RE = b'From \\S+' + DAY_RE + MON_RE + DTY_RE + b'( .+)?' + linesep + b'\\Z'
     # we capture the optional moreinfo group so we can check for lines that end in the date
 
     _mangle_from_ = True
